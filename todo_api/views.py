fa5cd7fa400b39de9b7958713c4bbcd4b38addd9
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-28 23:35:51 +0400

    created view for HTTP handling

--- todo_api/views.py
+++ todo_api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializer import TaskSerializer
 
-# Create your views here.
+# Creating a view for HTTP handling
+# using ModelViewSet as it has built-in functions for handling api requests such as create, retrive, delete
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all() # grab all items in Task
+    serializer_class = TaskSerializer
